45be2454f96038356815540d2cbad3e564a6e57c
sujalkrg <sujalkrghosh@gmail.com>
2025-06-12 23:26:38 +0530

    Added Schema and post method

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://root:root@books-store-mern.4uqzkym.mongodb.net/?retryWrites=true&w=majority&appName=Books-store-MERN";
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,55 @@
 import express from "express";
-import { PORT } from "./config.js";
+import mongoose from "mongoose";
+import { PORT, mongoDBURL } from "./config.js";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
-app.get('/', () => {
-    
+//Middleware of parsing request body
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome to MERN Stack Tutorial')
+});
+
+//Route for saving a new book
+
+app.post('/books', async (request, response) => {
+    try {
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+
+        const newBook = {
+        title: request.body.title,
+        author: request.body.author,
+        publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
 })
 
-app.listen(PORT, ()=>{
-    console.log(`App listening to port: ${PORT}`)
-})
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("DB connected");
+        app.listen(PORT, ()=>{
+        console.log(`App listening to port: ${PORT}`)
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });

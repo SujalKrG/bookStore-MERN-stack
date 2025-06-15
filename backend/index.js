4105c27d021126e0e6d8be0b708eaba4c4297bce
sujalkrg <sujalkrghosh@gmail.com>
2025-06-15 13:36:02 +0530

    Added refactor and frontend setup with tailwind

--- backend/index.js
+++ backend/index.js
@@ -2,118 +2,33 @@ import express, { request, response } from "express";
 import mongoose from "mongoose";
 import { PORT, mongoDBURL } from "./config.js";
 import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
-//Middleware of parsing request body
+//Middleware for parsing request body
 app.use(express.json());
+
+//Middleware for handling cors policy
+//Option 1: Allow all origins with default cors (*)
+// app.use(cors());
+
+//Option 2: Allow custom origins
+app.use(
+    cors({
+        origin: "http://localhost:5555",
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    })
+)
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome to MERN Stack Tutorial')
 });
 
-//Route for saving a new book
-app.post('/books', async (request, response) => {
-    try {
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields: title, author, publishYear',
-            });
-        }
-
-        const newBook = {
-        title: request.body.title,
-        author: request.body.author,
-        publishYear: request.body.publishYear,
-        };
-
-        const book = await Book.create(newBook);
-
-        return response.status(201).send(book);
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-//Route for getting all books from database
-app.get('/books', async(request, response) => {
-    try {
-        const books = await Book.find({});
-
-        return response.status(200).json({
-            count: books.length,
-            data: books
-        });
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-//Route for getting one book from database by id
-app.get('/books/:id', async (request, response) => {
-    try {
-        const { id } = request.params;
-
-        const book = await Book.findById(id);
-
-        return response.status(200).json(book);
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-});
-
-//Route for updating a book
-app.put('/books/:id', async (request, response) => {
-    try {
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: "Send all required fields: title, author, publish year",
-            });
-        }
-
-        const { id } = request.params;
-
-        const result = await Book.findByIdAndUpdate(id, request.body);
-
-        if(!result){
-            return response.status(404).json({message: "Book not found"});
-        }
-
-        return response.status(200).send({message: "Book Updated Successfully"})
-    } catch (error) {
-        console.log(error.message);
-        return response.status(500).send({message: error.message});
-    }
-});
-
-//Route for deleting a book
-app.delete('/books/:id', async (request, response) => {
-    try {
-        const { id } = request.params;
-
-        const result = await Book.findByIdAndDelete(id);
-
-        if(!result){
-            return response.status(404).send({ message: "Book not found" });
-        }
-        return response.status(200).send({ message: "Book deleted Successfully" });
-    } catch (error) {
-        console.log(error.message);
-        return response.status(500).send(error.message);
-    }
-});
+app.use('/books', booksRoute);
 
 mongoose
     .connect(mongoDBURL)

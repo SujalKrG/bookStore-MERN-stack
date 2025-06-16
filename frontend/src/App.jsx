41d673429d8938170252a85a91131832cfa1ee3d
sujalkrg <sujalkrghosh@gmail.com>
2025-06-16 21:45:11 +0530

    Added all basic page ccomponents

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import ShowBooks from './pages/showBooks'
+import EditBook from './pages/editBook'
+import DeleteBook from './pages/DeleteBook'
+import CreateBooks from './pages/CreateBooks'
+
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/create" element={<CreateBooks />} />
+      <Route path="/books/details/:id" element={<ShowBooks />} />
+      <Route path="/books/edit/:id" element={<EditBook />} />
+      <Route path="/books/delete/:id" element={<DeleteBook />} />
+    </Routes>
   )
 }
 
---
+++ frontend/src/components/BackButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import {BsArrowLeft} from "react-icons/bs"
+
+const BackButton = () => {
+  return (
+    <div>BackButton</div>
+  )
+}
+
+export default BackButton

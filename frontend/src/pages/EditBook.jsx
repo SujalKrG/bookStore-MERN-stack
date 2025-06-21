e089c4a94ce0d757d83f49b629703fcc0e756024
Sujal Kumar Ghosh <131712651+SujalKrG@users.noreply.github.com>
2025-06-21 12:50:30 +0530

    Update EditBook.jsx (bug fixed)
    There was a short error ... i didnt change the localhost to our base backend url

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -37,7 +37,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`http://localhost:5555/books/${id}`, data)
+      .put(`${BASE_URL}/books/${id}`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book edited successfully', { variant: 'success' });
